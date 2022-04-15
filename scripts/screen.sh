4c6a4b1e10fe25cd3669f243ea7fdbdbb99fcc6e
3llena-arch <30310224+3llena-arch@users.noreply.github.com>
2022-04-15 20:44:57 +0200

    removed verif in screen.sh

--- scripts/screen.sh
+++ scripts/screen.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-# verify vars
-if [[ -z $CONFIG_PRIMARY_SCREEN ]] || [[ -z $CONFIG_PRIMARY_MODE ]]; then exit; fi
-
 # create mode
 xrandr --newmode $(cvt $CONFIG_PRIMARY_MODE | grep Modeline | awk '{$1=""}1')
 xrandr --addmode $CONFIG_PRIMARY_SCREEN $(cvt $CONFIG_PRIMARY_MODE | grep Modeline | awk '{print $2}')
