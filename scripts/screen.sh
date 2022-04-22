1b2caca678c5333c35b831807c94fe558027c72c
3llena-arch <30310224+3llena-arch@users.noreply.github.com>
2022-04-22 20:53:01 +0200

    updated screen

--- scripts/screen.sh
+++ scripts/screen.sh
@@ -5,5 +5,4 @@ if [[ -z $CONFIG_PRIMARY_SCREEN ]] || [[ -z $CONFIG_PRIMARY_MODE ]]; then exit; 
 
 # create mode
 xrandr --newmode $(cvt $CONFIG_PRIMARY_MODE | grep Modeline | awk '{$1=""}1')
-xrandr --addmode $CONFIG_PRIMARY_SCREEN $(cvt $CONFIG_PRIMARY_MODE | grep Modeline | awk '{print $2}')
-xrandr --output $CONFIG_PRIMARY_SCREEN --mode $(cvt $CONFIG_PRIMARY_MODE | grep Modeline | awk '{print $2}')
+xrandr -s $(cvt $CONFIG_PRIMARY_MODE | grep Modeline | awk '{print $2}' | cut -d "\"" -f 2)
