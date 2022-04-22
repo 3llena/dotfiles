d170eb325c3b296a2e35f24a6a9c569947669c63
3llena-arch <30310224+3llena-arch@users.noreply.github.com>
2022-04-22 20:53:39 +0200

    added more cleanup smh

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -4,6 +4,7 @@
 while :; do
   rm -rf $HOME/.cache $HOME/.bash_history $HOME/.config/neofetch $HOME/.config/procps
   rm -rf $HOME/Desktop $HOME/Downloads $HOME/Documents $HOME/Pictures $HOME/Videos
-  rm -rf $HOME/.mozilla $HOME/.hyperbola $HOME/.gitconfig
+  rm -rf $HOME/.mozilla $HOME/.hyperbola $HOME/.gitconfig $HOME/.lesshst $HOME/.local
+  rm -rf $HOME/.waterfox
   sleep 1m
 done
