a6ce2d631320af98b82c479aceb525dd6b6be279
3llena-arch <this.transform.identity@gmail.com>
2022-04-22 12:57:35 +0200

    updated dotfiles and added wallpapers, scripts and startup

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -4,6 +4,6 @@
 while :; do
   rm -rf $HOME/.cache $HOME/.bash_history $HOME/.config/neofetch $HOME/.config/procps
   rm -rf $HOME/Desktop $HOME/Downloads $HOME/Documents $HOME/Pictures $HOME/Videos
-  rm -rf $HOME/.mozilla $HOME/.hyperbola
+  rm -rf $HOME/.mozilla $HOME/.hyperbola $HOME/.gitconfig
   sleep 1m
 done
--- scripts/screen.sh
+++ scripts/screen.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# verify vars
+if [[ -z $CONFIG_PRIMARY_SCREEN ]] || [[ -z $CONFIG_PRIMARY_MODE ]]; then exit; fi
+
 # create mode
 xrandr --newmode $(cvt $CONFIG_PRIMARY_MODE | grep Modeline | awk '{$1=""}1')
 xrandr --addmode $CONFIG_PRIMARY_SCREEN $(cvt $CONFIG_PRIMARY_MODE | grep Modeline | awk '{print $2}')
